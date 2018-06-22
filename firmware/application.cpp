768d13e6e07f140499038bee3b005a8d3d5c7909
John Wass <jwass3@gmail.com>
2018-06-21 22:47:29 -0400

    clean up impl

--- firmware/application.cpp
+++ firmware/application.cpp
@@ -1,31 +1,37 @@
 #include <Particle.h>
 #include <TinyGPS++.h>
 
-long lastEvent = 0;
-int eventInterval = 5 * 1000;
+constexpr int buffer_sz = 32;
+
+system_tick_t lastEvent = 0;
+system_tick_t eventInterval = 5000;
+std::array<char, buffer_sz> buffer;
 
 TinyGPSPlus gps;
 
 void setup() {
    Serial1.begin(38400);
-   Particle.publish("ready", PRIVATE);
+   Particle.publish("R", PRIVATE);
 }
 
 void loop() {
    while(Serial1.available() > 0) {
-      auto v = Serial1.read();
-      if(gps.encode(v)) {
-         if(millis() - lastEvent > eventInterval) {
-            auto location = gps.location;
-            if(location.isValid() && location.isUpdated()) {
-               auto lat = String(location.lat(), 6);
-               auto lon = String(location.lng(), 6);
-               auto str = String::format("%s:%s", lat.c_str(), lon.c_str());
+      if(Serial1.readBytes(buffer.data(), buffer_sz)) {
+         bool exec = millis() - lastEvent > eventInterval;
+         for(const auto& c: buffer) {
+            if(gps.encode(c) && exec) {
+               auto location = gps.location;
+               if(location.isValid() && location.isUpdated()) {
+                  auto lat = String(location.lat(), 6);
+                  auto lon = String(location.lng(), 6);
+                  auto str = String::format("%s:%s", lat.c_str(), lon.c_str());
 
-               lastEvent = millis();
-               Particle.publish("pos", str, PRIVATE);
+                  lastEvent = millis();
+                  Particle.publish("G", str, PRIVATE);
+               }
             }
          }
       }
+      buffer = {};
    }
 }
