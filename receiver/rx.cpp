2093a261eea164a7a4fcfadad16d1a151df4e900
John Wass <jwass3@gmail.com>
2019-10-17 19:04:29 -0400

    add serial relay to receiver

--- receiver/rx.cpp
+++ receiver/rx.cpp
@@ -1,2 +1,12 @@
-void loop() {
+#include <Particle.h>
+
+constexpr int serial1br = 38400;
+
+void transmit(const char* e, const char* d) {
+   Serial1.printlnf("%s:%s", e, d);
+}
+
+void setup() {
+   Particle.subscribe("M", transmit, MY_DEVICES);
+   Serial1.begin(serial1br);
 }
