e806f50d486dedd425b783a8866ceadd3fac9898
John Wass <jwass3@gmail.com>
2019-10-21 22:40:49 -0400

    switching to neogps

--- transmitter/tx.cpp
+++ transmitter/tx.cpp
@@ -1,5 +1,4 @@
 #include <Particle.h>
-#include <TinyGPS++.h>
 #include <functional>
 #include <experimental/optional>
 
@@ -7,14 +6,24 @@
 #include <LIS3DH.h>
 #endif
 
+#include <NMEAGPS.h>
+#include <GPSport.h>
+
+auto gpsPort = Serial1;
+
+constexpr uint8_t ExternalAntenna[] = {0xB5, 0x62, 0x06, 0x13, 0x04, 0x00, 0x01, 0x00, 0xF0, 0x7D, 0x8B, 0x2E};
+
 using meters = double;
 using tick = system_tick_t;
+
 using std::experimental::nullopt;
 using std::experimental::optional;
 using std::experimental::make_optional;
-using OptLocation = optional<TinyGPSLocation>;
 
-bool movedAtLeast(meters, TinyGPSLocation&, TinyGPSPlus&);
+using Location = NeoGPS::Location_t;
+using OptLocation = optional<Location>;
+
+bool movedAtLeast(meters, Location&, Location&);
 bool waitedAtLeast(tick, tick, const std::function<tick(void)>&);
 
 SYSTEM_THREAD(ENABLED)
@@ -34,8 +43,6 @@ tick eventInterval = 5000;
 OptLocation lastPos = nullopt;
 meters moveThreshold = 2;
 
-TinyGPSPlus gps;
-
 const String Id = String(CloudClass::deviceID()).substring(0, 6);
 const String ReadyEvent = String("R/") + Id;
 const String MovedEvent = String("M/") + Id;
@@ -44,46 +51,46 @@ const String MovedEvent = String("M/") + Id;
 LIS3DHSPI accel(SPI, A2, WKP);
 #endif
 
+
 void setup() {
-   Serial1.begin(serial1br);
+   gpsPort.begin(serial1br);
 
 #ifdef ASSET_TRACKER
    // turn on the asset tracker gps
    pinMode(D6, OUTPUT);
    digitalWrite(D6, LOW);
+   gpsPort.write(ExternalAntenna, sizeof(ExternalAntenna));
 #endif
 
    Particle.publish(ReadyEvent, PRIVATE);
 }
 
-void serialEvent1() {
-   Serial1.readBytes(buffer.data(), buffer_sz);
-   for(const auto& c: buffer) {
-      if(gps.encode(c) && waitedAtLeast(lastEvent, eventInterval, millis)) {
-         auto location = gps.location;
-         if(location.isValid() && location.isUpdated()) {
-            if(!lastPos || movedAtLeast(moveThreshold, *lastPos, gps)) {
-               auto lat = String(location.lat(), 6);
-               auto lon = String(location.lng(), 6);
-               auto str = String::format("%s:%s", lat.c_str(), lon.c_str());
+//--------------------------
 
-               lastEvent = millis();
-               lastPos = make_optional(location);
-               Particle.publish(MovedEvent, str, PRIVATE);
-            }
+NMEAGPS gps;
+gps_fix fix;
+void loop() {
+   while(gps.available(gpsPort)) {
+      fix = gps.read();
+
+      if(fix.valid.location && waitedAtLeast(lastEvent, eventInterval, millis)) {
+         auto l = gps.fix().location;
+         if(!lastPos || movedAtLeast(moveThreshold, *lastPos, l)) {
+            auto lat = String(l.latF(), 6);
+            auto lon = String(l.lonF(), 6);
+            auto str = String::format("%s:%s", lat.c_str(), lon.c_str());
+
+            lastEvent = millis();
+            lastPos = make_optional(l);
+            Particle.publish(MovedEvent, str, PRIVATE);
          }
       }
    }
-   buffer = {};
 }
 
-void loop() {
-}
 
-bool movedAtLeast(meters m, TinyGPSLocation& from, TinyGPSPlus& gps) {
-   return m <= TinyGPSPlus::distanceBetween(
-         gps.location.lat(), gps.location.lng(),
-         from.lat(), from.lng());
+bool movedAtLeast(meters m, Location& from, Location& to) {
+   return m <= from.DistanceKm(to) * 1000;
 }
 
 bool waitedAtLeast(tick prev, tick interval, const std::function<tick(void)>& t) {
