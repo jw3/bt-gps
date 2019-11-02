608f952f1f3a6ac2e038af1f0e9e372a944ef349
John Wass <jwass3@gmail.com>
2019-11-01 22:05:38 -0400

    attempt to prevent stuck connections

--- transmitter/tx.cpp
+++ transmitter/tx.cpp
@@ -20,8 +20,6 @@ bool waitedAtLeast(tick, tick, const std::function<tick(void)>&);
 
 SYSTEM_THREAD(ENABLED)
 
-
-
 constexpr int buffer_sz = 32;
 std::array<char, buffer_sz> buffer;
 
@@ -30,6 +28,9 @@ gps_fix fix;
 
 tick lastEvent = 0;
 tick eventInterval = 5000;
+
+tick connectingSince = 0;
+tick disconnectedAt = 0;
 
 OptLocation lastPos = nullopt;
 meters moveThreshold = 2;
@@ -43,21 +44,41 @@ void setup() {
    Particle.publish(ReadyEvent, PRIVATE);
 }
 
+// state machine ...
 void loop() {
-   while(gps.available(gpsPort)) {
-      fix = gps.read();
+   if(Cellular.ready()) {
+      if(connectingSince) {
+         // log?
+         connectingSince = 0;
+      }
 
-      if(fix.valid.location) {
-         if(!lastPos || movedAtLeast(moveThreshold, *lastPos, fix.location) || waitedAtLeast(lastEvent, eventInterval, millis)) {
-            auto lat = String(fix.location.latF(), 6);
-            auto lon = String(fix.location.lonF(), 6);
-            auto mph = String(static_cast<uint8_t>(fix.speed_mph()));
-            auto str = String::format("%s:%s:%s", lat.c_str(), lon.c_str(), mph.c_str());
+      while(gps.available(gpsPort)) {
+         fix = gps.read();
 
-            lastEvent = millis();
-            lastPos = make_optional(fix.location);
-            Particle.publish(MovedEvent, str, PRIVATE);
+         if(fix.valid.location) {
+            if(!lastPos || movedAtLeast(moveThreshold, *lastPos, fix.location) || waitedAtLeast(lastEvent, eventInterval, millis)) {
+               auto lat = String(fix.location.latF(), 6);
+               auto lon = String(fix.location.lonF(), 6);
+               auto mph = String(static_cast<uint8_t>(fix.speed_mph()));
+               auto str = String::format("%s:%s:%s", lat.c_str(), lon.c_str(), mph.c_str());
+
+               lastEvent = millis();
+               lastPos = make_optional(fix.location);
+               Particle.publish(MovedEvent, str, PRIVATE);
+            }
          }
+      }
+   }
+   else {
+      if(connectingSince && waitedAtLeast(connectingSince, 5000, millis)) {
+         Cellular.disconnect();
+         disconnectedAt = millis();
+         connectingSince = 0;
+      }
+      else if(disconnectedAt && waitedAtLeast(disconnectedAt, 1000, millis)) {
+         Cellular.connect();
+         connectingSince = millis();
+         disconnectedAt = 0;
       }
    }
 }
